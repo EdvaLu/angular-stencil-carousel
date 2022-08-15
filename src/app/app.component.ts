85af80d39b97bbe007c188138a354ca1ca331ac3
EdvaLu <edvardaslunys@gmail.com>
2022-08-15 17:59:46 +0100

    Import stencil component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import CarouselData from '../mock/data.js';
+
+interface CarouselItem {
+    name?: string;
+    image: string;
+    altText?: string;
+}
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-stencil-carousel';
+    data: CarouselItem[] = CarouselData;
+    interval: number = 3000;
+
+    onClickImage(): void {
+        console.log("Image was clicked");
+    }
+
+    onClickNextPrevButton($event): void {
+        console.log($event.detail);
+    }
+
+    onClickDotButton($event): void {
+        console.log($event.detail);
+    }
 }
---
+++ src/mock/data.js
@@ -0,0 +1,17 @@
+export default [
+	{
+		name: "Buddy",
+		image: "https://post.medicalnewstoday.com/wp-content/uploads/sites/3/2020/02/322868_1100-800x825.jpg",
+		altText: "Lorem Ipsum is simply dummy text of the printing and typesetting industry"
+	},
+	{
+		name: "Ruger",
+		image: "https://www.collinsdictionary.com/images/full/dog_230497594.jpg",
+		altText: "There are many variations of passages of Lorem Ipsum available"
+	},
+	{
+		name: "Leo",
+		image: "https://arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/HB4AT3D3IMI6TMPTWIZ74WAR54.jpg",
+		altText: ""
+	}
+]
